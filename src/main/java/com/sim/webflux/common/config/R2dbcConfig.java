1c2dd5b4334aa2f13a58eb1d4e87be081ead6ede
tlarbals824 <simkm0824@naver.com>
2023-07-08 00:08:17 +0900

    [feat] R2DBC basePackage 설정 추가

--- src/main/java/com/sim/webflux/common/config/R2dbcConfig.java
+++ src/main/java/com/sim/webflux/common/config/R2dbcConfig.java
@@ -2,8 +2,10 @@ package com.sim.webflux.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
 @EnableR2dbcAuditing
-public class R2dbcConfig {
+@EnableR2dbcRepositories(basePackages = "com.sim.webflux.r2dbc")
+public class R2dbcConfig{
 }
